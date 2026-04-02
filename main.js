e3330d8b203d7ebce8de450b538430c40f38deba
Bhimavarpu Mohith Sai <bmstpt1@gmail.com>
2026-04-02 12:25:28 +0530

    Update greeting from 'Hello' to 'Goodbye'

--- main.js
+++ main.js
@@ -847,5 +847,13 @@ screenshotBtn.addEventListener("click", () => {
 
   window.addEventListener("resize", adjustLayoutForViewport);
   adjustLayoutForViewport();
+  if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("service-worker.js")
+      .then(() => console.log("Service Worker Registered"))
+      .catch((err) => console.log("SW Error:", err));
+  });
+}
 });
 
